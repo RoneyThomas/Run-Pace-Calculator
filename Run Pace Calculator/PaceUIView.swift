c2dec6772a31c614f23130bd461a53972b68f2a4
RoneyThomas <roneythomas6@gmail.com>
2020-04-08 17:56:55 -0400

    Sorry these commit messages aren't any meaningful, right now just working on a proof of concept

--- Run Pace Calculator/PaceUIView.swift
+++ Run Pace Calculator/PaceUIView.swift
@@ -7,21 +7,56 @@
 //
 
 import SwiftUI
-import Combine
+import Foundation
 
 struct PaceUIView: View {
     var units = ["min/km", "min/mi"]
     @State private var selectionUnit: Int = 0
-    @State private var minute: String = "4"
-    @State private var second: String = "18"
+    @State private var minute_TextField: String = "4"
+    @State private var second_TextField: String = "18"
+    @State private var seconds: Int = 258
+    fileprivate func getTime(distnace: Float) -> String {
+        let interval = round((distnace+1) * Float(seconds))
+        
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        
+        let formattedString = formatter.string(from: TimeInterval(interval))!
+        print(formattedString)
+        return formattedString
+    }
     
     var body: some View {
-        NavigationView {
+        let pi = Binding<Int>(get: {
+            return self.selectionUnit
+        }, set: {
+            var interval: Int = 0
+            let formatter = DateComponentsFormatter()
+            formatter.unitsStyle = .positional
+            formatter.allowedUnits = [.minute, .second]
+            if self.selectionUnit == 0 && $0 == 1{
+                self.selectionUnit = $0
+                interval = Int(round(1.609344 * Float(self.seconds)))
+            } else if self.selectionUnit == 1 && $0 == 0 {
+                self.selectionUnit = $0
+                interval = Int(round(0.62137119223733 * Float(self.seconds)))
+            }
+            self.seconds = interval
+            let formatted_string = (formatter.string(from: TimeInterval(interval))!).split(separator: ":")
+            if formatted_string.count == 1 {
+                self.second_TextField = String(formatted_string[0])
+            } else if formatted_string.count == 2 {
+                self.minute_TextField = String(formatted_string[0])
+                self.second_TextField = String(formatted_string[1])
+            }
+        })
+        return NavigationView {
             ZStack(alignment: .topLeading) {
                 Color(UIColor.systemGray6).edgesIgnoringSafeArea(.all)
                 VStack {
                     VStack {
-                        Picker("Units", selection: $selectionUnit) {
+                        Picker("Units", selection: pi) {
                             ForEach(0..<units.count) { index in
                                 Text(self.units[index])
                                     .tag(index)
@@ -30,18 +65,19 @@ struct PaceUIView: View {
                             .padding(.bottom)
                         HStack {
                             VStack {
-                                TextField("5", text: $minute)
+                                TextField("5", text: $minute_TextField)
                                     .keyboardType(.numberPad)
                                     .textFieldStyle(RoundedBorderTextFieldStyle())
                             }.frame(minWidth: 0, maxWidth: .infinity)
                             VStack {
-                                TextField("36", text: $second)
+                                TextField("36", text: $second_TextField)
                                     .keyboardType(.numberPad)
                                     .textFieldStyle(RoundedBorderTextFieldStyle())
                             }.frame(minWidth: 0, maxWidth: .infinity)
                             VStack {
                                 Button("Set Pace", action: {
                                     UIApplication.shared.endEditing()
+                                    self.seconds = ((Int(self.minute_TextField) ?? 0)*60) + (Int(self.second_TextField) ?? 0)
                                 })
                             }.frame(minWidth: 0, maxWidth: .infinity)
                         }
@@ -52,7 +88,7 @@ struct PaceUIView: View {
                                 Text("Dist \(selectionUnit == 0 ? "km" : "mi")").font(.headline)
                             }.frame(minWidth: 0, maxWidth: .infinity / 2)
                             VStack {
-                                Text("\(minute)" + ":" + "\(second)").font(.headline)
+                                Text("\(minute_TextField)" + ":" + "\(second_TextField)").font(.headline)
                             }.frame(minWidth: 0, maxWidth: .infinity / 2)
                         }.padding(.vertical)) {
                             if selectionUnit == 0 {
@@ -62,7 +98,7 @@ struct PaceUIView: View {
                                             Text("\(index + 1)").fontWeight(.semibold)
                                         }.frame(minWidth: 0, maxWidth: .infinity / 2)
                                         VStack {
-                                            Text("\(self.minute):\(self.second)").fontWeight(.semibold)
+                                            Text(self.getTime(distnace: Float(index))).fontWeight(.semibold)
                                         }.frame(minWidth: 0, maxWidth: .infinity / 2)
                                     }
                                 }
@@ -73,7 +109,7 @@ struct PaceUIView: View {
                                             Text("\(index + 1)").fontWeight(.semibold)
                                         }.frame(minWidth: 0, maxWidth: .infinity / 2)
                                         VStack {
-                                            Text("\(self.minute):\(self.second)").fontWeight(.semibold)
+                                            Text(self.getTime(distnace: Float(index))).fontWeight(.semibold)
                                         }.frame(minWidth: 0, maxWidth: .infinity / 2)
                                     }
                                 }
@@ -88,7 +124,7 @@ struct PaceUIView: View {
                 //                        .modifier(AdaptsToSoftwareKeyboard())
                 //                    )
             }
-                .navigationBarTitle(Text("Pace Calculator"))
+            .navigationBarTitle(Text("Pace Calculator"))
         }
     }
 }
